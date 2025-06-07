dd69f3be0b0d0b702e3c9bcbc79e896724acc7e7
Musungu2 <ruthmusungu2019@gmail.com>
2025-06-07 07:20:59 -0700

    datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -10,6 +10,3 @@ def calculate_future_date():
     current_date = datetime.datetime.now()
     future_date = current_date + datetime.timedelta(days=days)
     print("Future Date:", future_date.strftime("%Y-%m-%d"))
-
-display_current_datetime()
-calculate_future_date()
