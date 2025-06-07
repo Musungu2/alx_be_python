3306e10603bf3ab19d4c373edbd0dc387133c4f0
Musungu2 <ruthmusungu2019@gmail.com>
2025-06-07 07:03:12 -0700

    task2

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,13 @@
+import datetime
+
+def  display_current_datetime():
+    current_date = datetime.datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current Date and Time:", formatted_date)
+
+def calculate_future_date():
+    days= int(input("Enter number of days: "))
+    current_date = datetime.datetime.now()
+    future_date = current_date + days.timedelta(days=days)
+    printrint("Future Date:", future_date.strftime("%Y-%m-%d"))
+
